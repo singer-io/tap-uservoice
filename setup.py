ec6548e6f0145726d16f8ff2f14ffa39e2f21279
Nick McCoy <nick@stitchdata.com>
2018-07-19 15:43:44 -0400

    bump version of singer-python

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='tap-uservoice',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_uservoice'],
       install_requires=[
-          'singer-python==5.0.12',
+          'singer-python==5.1.5',
           'backoff==1.3.2',
           'requests==2.18.4',
           'funcy==1.10.1',
